74187a5b5e528ca6cb3313775504b54b14c4a787
MaximFrontend <maximrus62@gmail.com>
2023-07-23 15:54:42 +0300

    gulp template v1.0.0

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const config = {
+   mode: 'production', // Определяем режим работы - production, еще бывает режим работы - development
+
+   entry: {
+      index: './src/js/index.js',      // Здесь указывается файл, который мы будем обрабатывать. Для каждой страницы сайта свой JS файл. Название и путь
+      // contacts: './src/js/contacts.js',
+      // about: './src/js/about.js',
+   },
+
+   output: {
+      filename: '[name].bundle.js', // В name попадает имя файла JS. Например contacts.js
+   },
+
+   module: {
+      rules: [
+         {
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader'],
+         },
+      ],
+   },
+};
+
+module.exports = config;
